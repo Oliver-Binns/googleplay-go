a5f69172d08f50b6a58cf098101c1f9b0714dd79
Oliver Binns <mail@oliverbinns.co.uk>
2025-04-08 17:14:41 +0100

    feat: add service account representation (#4)

---
+++ authorization/service_account_provider.go
@@ -0,0 +1,79 @@
+package authorization
+
+import (
+	"crypto/rsa"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type TokenSource interface {
+	Token() (string, error)
+}
+
+func NewTokenSource(account ServiceAccount) (TokenSource, error) {
+	pk, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(account.PrivateKey))
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse private key: %w", err)
+	}
+
+	return &tokenSource{
+		account:   account,
+		pk:        pk,
+		expiresIn: time.Hour,
+	}, nil
+}
+
+type tokenSource struct {
+	sync.Mutex
+
+	account   ServiceAccount
+	pk        *rsa.PrivateKey
+	expiresIn time.Duration
+	bearer    string
+	expireAt  time.Time
+}
+
+func (ts *tokenSource) Token() (string, error) {
+	ts.Lock()
+	defer ts.Unlock()
+
+	if ts.isExpired() {
+		return ts.refresh()
+	}
+
+	return ts.bearer, nil
+}
+
+func (ts *tokenSource) isExpired() bool {
+	return time.Now().After(ts.expireAt)
+}
+
+func (ts *tokenSource) refresh() (string, error) {
+	// Create JWT as defined in https://developers.google.com/identity/protocols/oauth2/service-account
+	iat := time.Now()
+	exp := iat.Add(ts.expiresIn)
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
+		"iss": ts.account.ClientEmail,
+		"sub": ts.account.ClientEmail,
+		"aud": "https://firestore.googleapis.com/",
+		"iat": iat.Unix(),
+		"exp": exp.Unix(),
+	})
+	token.Header["alg"] = "RS256"
+	token.Header["typ"] = "JWT"
+	token.Header["kid"] = ts.account.PrivateKeyID
+
+	bearer, err := token.SignedString(ts.pk)
+	if err != nil {
+		return "", fmt.Errorf("failed to sign token: %w", err)
+	}
+
+	ts.bearer = bearer
+	ts.expireAt = exp
+
+	return bearer, nil
+}
--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/oliver-binns/googleplay-go
 
 go 1.22
 
-require (
-
-)
+require github.com/golang-jwt/jwt/v5 v5.2.1
+require github.com/stretchr/testify v1.9.0
+require github.com/davecgh/go-spew v1.1.1 // indirect
+require github.com/pmezard/go-difflib v1.0.0 // indirect
+require gopkg.in/yaml.v3 v3.0.1 // indirect
