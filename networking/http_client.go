46691efba76a469543bfb073d968a56eb8144c69
Oliver Binns <mail@oliverbinns.co.uk>
2025-04-12 12:57:26 +0100

    feat: implement networking client

---
+++ networking/http_client.go
@@ -0,0 +1,46 @@
+package networking
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type TokenSource interface {
+	Token() (string, error)
+}
+
+type HTTPClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+type authorizedClient struct {
+	httpClient  HTTPClient
+	tokenSource TokenSource
+}
+
+func NewAuthorizedClient(httpClient HTTPClient, tokenSource TokenSource) HTTPClient {
+	return &authorizedClient{
+		httpClient:  httpClient,
+		tokenSource: tokenSource,
+	}
+}
+
+func (c *authorizedClient) Do(req *http.Request) (*http.Response, error) {
+	token, err := c.tokenSource.Token()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get token: %w", err)
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send request: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code %d", resp.StatusCode)
+	}
+
+	return resp, nil
+}
