f2da1c397f364584c5bdea3490076534c61639da
Oliver Binns <mail@oliverbinns.co.uk>
2025-04-13 12:22:45 +0100

    feat: delete users from Google Play Console

---
+++ users/delete.go
@@ -0,0 +1,30 @@
+package users
+
+import (
+	"context"
+	"fmt"
+	"googleplay-go/networking"
+	"net/http"
+	"net/url"
+	"path"
+)
+
+func Delete(c networking.HTTPClient, ctx context.Context, baseURL string, email string) error {
+	parsedURL, err := url.Parse(baseURL)
+	if err != nil {
+		return fmt.Errorf("failed to parse URL: %w", err)
+	}
+	parsedURL.Path = path.Join(parsedURL.Path, email)
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodDelete, parsedURL.String(), http.NoBody)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %w", err)
+	}
+
+	_, err = c.Do(req)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
